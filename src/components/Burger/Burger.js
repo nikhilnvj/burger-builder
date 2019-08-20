3c18e4c2719e75183a252a95c21de0c40696f423
Nikhil <nikhil.kumar.joshi@aa.com>
2019-08-19 23:59:08 -0400

    More/Less Buttons working.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,15 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientType => [...Array(props.ingredients[ingredientType])] // Create an array of ingredient size for each ing.
             .map((_, index) => <BurgerIngredient key={ingredientType + index} type={ingredientType}/> // Render for each ingredient
-            )
-        );
+            ))
+        .reduce((previousValue, currentValue) => previousValue.concat(currentValue), []); // Flatten the arrays
 
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
